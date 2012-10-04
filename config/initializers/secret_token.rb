54fdd9f7685d87944b5bef7d454cb79bf9f753b5
James Cox <th3james@gmail.com>
2012-10-04 13:55:17 +0100

    It's a rails project (surprise!)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NBSAPLanding::Application.config.secret_token = 'c5023741fea6795a71114b22a21997fc9b2fda274f1d3c9c279e15d0d35cc66a85b9d77d467844ad1c1a775034a2584ed97f93e9fef65f93d7acd43cbdcecfe8'
